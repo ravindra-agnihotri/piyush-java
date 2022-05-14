190e70a016eca81d40d5b9655b8d5dfb82a73a10
ravindra.agnihotri <ravindra.swt@gmail.com>
2022-05-14 08:33:49 +0530

    committing Father.java

---
+++ java_conecpts/src/oopSConcepts/inheritance/Father.java
@@ -0,0 +1,42 @@
+package oopSConcepts.inheritance;
+
+public class Father {
+
+    public static String color = "fair";
+
+    public String car = " BMW";
+
+    public void kheti(String bij){
+
+    }
+
+    public static void gramoPhone(){
+
+    }
+
+    public static void main(String[] args) {
+        gramoPhone();
+       Son son= new Son();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
